f0e02517c0313e389257541f835de0c8c7f0ad9d
Anand Mallick <anandmallick980@gmail.com>
2016-03-31 02:29:50 +0530

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ActiveRecord::Base
 	has_secure_password
 	validates :password, length: { minimum: 6 }, allow_blank: true
 
+	has_many :microposts, dependent: :destroy
+
 	# Return the hash digest of the given string
 	def self.digest(string)
 		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
@@ -37,8 +39,7 @@ class User < ActiveRecord::Base
 	end
 
 	def activate
-		update_attribute(:activated, true)
-		update_attribute(:activated_at, Time.zone.now)
+		update_columns(activated: true, activated_at: Time.zone.now)
 	end
 
 	def send_activation_email
@@ -47,8 +48,7 @@ class User < ActiveRecord::Base
 
 	def create_reset_digest
 		self.reset_token = User.new_token
-		update_attribute(:reset_digest, User.digest(reset_token))
-		update_attribute(:reset_sent_at, Time.zone.now)
+		update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
 	end
 
 	def password_reset_email
@@ -57,6 +57,10 @@ class User < ActiveRecord::Base
 
 	def password_reset_expired?
 		reset_sent_at < 2.hours.ago
+	end
+
+	def feed
+		Micropost.where("user_id = ?", id)
 	end
 
 	private
